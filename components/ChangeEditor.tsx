eb224f47471269a9d86d5b955a5d5c541136baf7
Kevin Huang <khu834@gmail.com>
2025-12-08 13:00:23 -0800

    backdrop remove

--- components/ChangeEditor.tsx
+++ components/ChangeEditor.tsx
@@ -108,18 +108,10 @@ export function ChangeEditor({ change, onSave, onCancel }: ChangeEditorProps) {
 
   return (
     <>
-      {/* Backdrop - very light, click to close */}
-      <div
-        data-testid="editor-backdrop"
-        className="absolute inset-0 bg-black/10 z-40 transition-opacity duration-300"
-        onClick={onCancel}
-      />
-
       {/* Side Panel - slides in from left, contained within image viewer */}
       <div
         data-testid="editor-panel"
         className="absolute top-0 left-0 h-full w-96 bg-white shadow-2xl z-50 transform transition-transform duration-300 ease-out flex flex-col border-r animate-slide-in-left"
-        onClick={(e) => e.stopPropagation()}
       >
         {/* Header */}
         <div className="p-4 border-b flex items-center justify-between shrink-0">
